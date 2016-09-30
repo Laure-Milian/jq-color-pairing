cda1853846033cfd03cdb41d7e2689de26954978
Laure <laure.milian@gmail.com>
2016-09-30 15:08:54 +0200

    fonds de couleur ok

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // good luck !
+
+var inputs = $("input");
+
+var len = inputs.length;
+
+for (i = 0; i < len; i++) {
+	var couleurs = ($(inputs[i]).data("color"));
+	$(inputs[i]).css("backgroundColor", couleurs);
+}
+
+
+
